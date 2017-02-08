52f2b943754f4c621bb68f4d728963d49f8c0233
V1nc3n7 <vincent.renard45@gmail.com>
2017-02-08 11:44:02 +0100

    it work 1.0

--- src/game/engine/Graphics.java
+++ src/game/engine/Graphics.java
@@ -5,7 +5,7 @@ import game.Component;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Graphics {
-
+/*
 	public static void quadData(int x, int y, int w, int h, float[] color) {
 		glColor4f(color[0], color[1], color[2], color[3]);
 		glVertex2f(x, y);
@@ -13,12 +13,66 @@ public class Graphics {
 		glVertex2f(x + w, y + h);
 		glVertex2f(x, y + h);
 	}
-
+	*/
+/*
 	public static void renderQuad(int x, int y, int w, int h, float[] color) {
 		glBegin(GL_QUADS);
 		Graphics.quadData(x, Component.height - y, w, h, color);
 		glEnd();
 	}
+	*/
 	
+	
+	public static void quadData(int x, int y, int w, int h, float[] color,int xO, int yO) {
+		float tailleGrille =32.0f;
+		glColor4f(color[0], color[1], color[2], color[3]);
+		glTexCoord2f(((0 + xO) / tailleGrille), (0 + yO) / tailleGrille);
+		glVertex2f(x, y);
+		glTexCoord2f(((1 + xO) / tailleGrille), (0 + yO) / tailleGrille);
+		glVertex2f(x + w, y);
+		glTexCoord2f(((1 + xO) / tailleGrille), (1 + yO) / tailleGrille);
+		glVertex2f(x + w, y + h);
+		glTexCoord2f(((0 + xO) / tailleGrille), (1 + yO) / tailleGrille);
+		glVertex2f(x, y + h);
+	}
+
+	public static void renderQuad(int x, int y, int w, int h, float[] color,int xO, int yO) {
+		glBegin(GL_QUADS);
+		quadData(x, y, w, h, color, xO, yO);
+		glEnd();
+	}
+
+	public static void quadData(int x, int y, int w, int h, float[] color) {
+		glColor4f(color[0], color[1], color[2], color[3]);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + w, y);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + w, y + h);
+		glTexCoord2f(1, 1);
+		glVertex2f(x, y + h);
+	}
+	public static void renderQuad(int x, int y, int w, int h, float[] color) {
+		glBegin(GL_QUADS);
+		quadData(x, y, w, h, color);
+		glEnd();
+	}
+	public static void render_entite(float x, float y, int w, int h, float[] color,float taille,int xO, int yO){
+		glBegin(GL_QUADS);
+		
+		glColor4f(color[0], color[1], color[2], color[3]);
+		glTexCoord2f(((0 + xO) / taille), (0 + yO) / taille);
+		glVertex2f(x, y);
+		glTexCoord2f(((1 + xO) / taille), (0 + yO) / taille);
+		glVertex2f(x + w, y);
+		glTexCoord2f(((1 + xO) / taille), (1 + yO) / taille);
+		glVertex2f(x + w, y + h);
+		glTexCoord2f(((0 + xO) / taille), (1 + yO) / taille);
+		glVertex2f(x, y + h);
+		
+		
+		glEnd();
+	}
 
 }
--- src/game/engine/Texture.java
+++ src/game/engine/Texture.java
@@ -13,6 +13,8 @@ import javax.imageio.ImageIO;
 
 import org.lwjgl.BufferUtils;
 
+import com.veridien.main.graphics.Texture;
+
 
 
 public class Texture {
@@ -30,57 +32,38 @@ public class Texture {
 
 	public static Texture chargementTexture(String s) {
 		BufferedImage image = null;
-		URL entree;
-		int larg, haut, startX, startY, scanSize, i, j, indice;
-		int[] RGBArray;
 		try {
-			image = null;
-			/*entree =Texture.class.getResource(s) ;
-			
-			*/
 			image = ImageIO.read(Texture.class.getResource(s));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		haut = image.getHeight();
-		larg = image.getWidth();
-		scanSize = larg;
-		startX = 0;
-		startY = 0;
-		RGBArray = new int[larg * haut];
-		image.getRGB(startX, startY, larg, haut, RGBArray, 0, scanSize);
-		ByteBuffer buffer = BufferUtils.createByteBuffer(larg * haut * 4);
-
-		for (j = 0; j < larg; j++) {
-			for (i = 0; i < haut; i++) {
-				indice = RGBArray[i + j * scanSize];
-
-				buffer.put((byte) ((indice) & 0xFF));
-				buffer.put((byte) ((indice >> 8) & 0xFF));
-				buffer.put((byte) ((indice >> 16) & 0xFF));
-				buffer.put((byte) ((indice >> 24) & 0xFF));
-/*
-				buffer.put((byte) (((RGBArray[i + j * scanSize])) & 0xFF));
-				buffer.put((byte) (((RGBArray[i + j * scanSize]) >> 8) & 0xFF));
-				buffer.put((byte) (((RGBArray[i + j * scanSize]) >> 16) & 0xFF));
-				buffer.put((byte) (((RGBArray[i + j * scanSize]) >> 24) & 0xFF));*/
-
+		int w = image.getWidth();
+		int h = image.getHeight();
+		int[] pixels = new int[w * h];
+		image.getRGB(0, 0, w, h, pixels, 0, w);
+		ByteBuffer buffer = BufferUtils.createByteBuffer(w * h * 4);
+		
+		for (int y = 0; y < w; y++) {
+			for (int x = 0; x < h; x++) {
+				int i = pixels[x + y * w];
+				buffer.put((byte) ((i >> 16) & 0xFF));
+				buffer.put((byte) ((i >> 8) & 0xFF));
+				buffer.put((byte) ((i) & 0xFF));
+				buffer.put((byte) ((i >> 24) & 0xFF));
 			}
 		}
-
-		 buffer.flip();
-
+		
+		buffer.flip();
+		
 		int id = glGenTextures();
 		glBindTexture(GL_TEXTURE_2D, id);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, larg, haut, 0, GL_RGBA,
-				GL_UNSIGNED_BYTE, buffer);
-
-		return new Texture(larg, haut, id);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+		
+		return new Texture(w, h, id);
 	}
 	public Texture DecTexture(int t, int size , String s){
 			BufferedImage image = null;
