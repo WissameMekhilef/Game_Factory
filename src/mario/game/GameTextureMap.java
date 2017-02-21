0dc39d79af80258c509f3c070523c2a818f8a342
V1nc3n7 <vincent.renard45@gmail.com>
2017-02-21 15:49:07 +0100

    Organisation textures + mapping

--- src/mario/game/GameTextureMap.java
+++ src/mario/game/GameTextureMap.java
@@ -14,20 +14,46 @@ import java.util.Map;
 public class GameTextureMap {
 
     public static Map<String , Texture> textureMap = new HashMap<>();
+    public static Map<String , Texture> backgroundMap = new HashMap<>();
     public static Map<String, Texture[]> skinMap = new HashMap<>();
 
     static {
     	try {
-			textureMap.put("brique", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/brique.png")));
-			textureMap.put("herbe", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/herbe.png")));
+    		//////BACKGROUND
+    		backgroundMap.put("bg_bleu_blanc_lum",  TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/background/background_blue_black_light.png")));
+    		backgroundMap.put("bg_arc_ciel_lum",  TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/background/background_rainbow_light.png")));
+    		backgroundMap.put("bg_arc_ciel_fum",  TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/background/background_rainbow_smoke.png")));
+
+    		//////TEXTURES
+			textureMap.put("nuage_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/clouds/nuage_1.png")));
+			textureMap.put("nuage_2", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/clouds/nuage_2.png")));
+			textureMap.put("nuage_3", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/clouds/nuage_3.png")));
+			textureMap.put("nuage_4", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/clouds/nuage_4.png")));
+			//textures
+    		textureMap.put("brique_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/brique.png")));
+    		textureMap.put("brique_2", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/brique_1.png")));
+    		textureMap.put("bois_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/wood_1.png")));
+    		textureMap.put("bois_2", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/wood_2.png")));
+    		textureMap.put("bois_3", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/wood_3.png")));
+    		textureMap.put("herbe", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/textures/brique.png")));
 	        //A changer
-	        textureMap.put("pause", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/pause.png")));
+	        textureMap.put("pause_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/menu/pause.png")));
 	        //Doors
-	        textureMap.put("door_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/door.png")));
+	        textureMap.put("porte_simple", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/doors/door.png")));
+	        textureMap.put("porte_b_n", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/doors/door_black_white.png")));
+	        textureMap.put("porte_metal", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/doors/door_metal.png")));
+	        textureMap.put("porte_bois_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/doors/door_wood_1.png")));
+	        textureMap.put("porte_bois_2", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/doors/door_wood_2.png")));
 	        //Coins
-	        textureMap.put("coin_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/pause.png")));
-	        skinMap.put("player1", new Texture[]{TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player.png")) , TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player_b.png"))});
-	        skinMap.put("flappy", new Texture[]{TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/flappy.png")) , TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/flappy_b.png"))});
+	        //textureMap.put("coin_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/pause.png")));
+	        //items
+	        textureMap.put("coin_1", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/items/coin_1.png")));
+	        textureMap.put("coin_2", TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/items/coin_2.png")));
+	        /////////PLAYERS
+	        //mario
+	        skinMap.put("player1", new Texture[]{TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player/player.png")) , TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player/player_b.png"))});
+	        //flappy
+	        skinMap.put("flappy", new Texture[]{TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player/flappy.png")) , TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("textures/player/flappy_B.png"))});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
